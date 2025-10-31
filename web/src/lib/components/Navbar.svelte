342dd0c4001bed74ef6251af513539e5d3ddf9ad
Wizard <p.swoyam.1@gmail.com>
2025-10-31 20:38:57 +0545

    Initial Setup
    - Pocketbase
    - Daisyui
    - View Transitions
    - Tailwindcss

---
+++ web/src/lib/components/Navbar.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+</script>
+
+<nav class="bg-base-100 py-8" style="view-transition-name: navbar-transition;">
+    <div class="container-grid">
+        <div class="grid-12">
+            <!-- Logo -->
+            <div class="col-span-2 items-center sm:col-span-4 lg:col-span-3">
+                <a class="geist uppercase text-xl font-semibold" href="/">
+                    TODO
+                </a>
+            </div>
+            <!-- Nav links -->
+            <div class="hidden lg:col-span-6 lg:flex justify-center">
+                <ul class="flex gap-6">
+                    <li><a href="/">Link 1</a></li>
+                    <li><a href="/">Link 2</a></li>
+                </ul>
+            </div>
+            <!-- Auth -->
+            <div
+                class="col-span-2 sm:col-span-4 lg:col-span-3 flex gap-2 justify-end"
+            >
+                <a
+                    class="btn btn-sm btn-ghost invisible md:visible"
+                    href="/auth/register">Sign Up</a
+                >
+                <a class="btn btn-primary btn-sm" href="/auth/login"
+                    >Login &nearr;</a
+                >
+            </div>
+        </div>
+    </div>
+</nav>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="heading morganite text-[4rem~16rem] font-bold uppercase">Hackathon 01</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
